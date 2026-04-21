b7791684986cbe46bb33b3c4077aaf588f28294f
ChristianB82 <cbahamon1@student.ccc.edu>
2026-04-21 10:07:27 -0500

    Add files via upload
    P1 Variable: Christian B

---
+++ 01pvariable.py
@@ -0,0 +1,15 @@
+#@ChristianBahamon
+
+
+
+fname = input('Write your first name here: ')
+lname = input('Write your last name here: ')
+address = input('Write your address here: ')
+city = input('Write your city here: ')
+scode = input('Write your state code here: ')
+pcode = input('Write your postal code here: ')
+
+print('\n'*2)
+print(fname + ' ' + lname)
+print(address)
+print(city + ', ' + scode + ' ' + pcode)
