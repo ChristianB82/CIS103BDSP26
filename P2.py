eaf14080fa883d038ab323e62739528afa9946e0
ChristianB82 <cbahamon1@student.ccc.edu>
2026-04-21 10:09:13 -0500

    Add files via upload

---
+++ P2.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  4 21:28:51 2026
+
+@author: Christian Bahamon
+"""
+
+('\n')
+print('Area Calculator')
+print('\n'*1)
+
+print('Rectangle')
+length = float(input('Length: '))
+width = float(input('Width: '))
+print('area' , length * width)
+print('\n'*1)
+
+print('Triangle')
+base = float(input('Base: '))
+height = float(input('Height: '))
+print('area' , 0.5 * base * height)
+print('\n'*1)
+
+print('Circle')
+radius = float(input('Radius: '))
+print('Area', 3.14 * radius * radius)
+
